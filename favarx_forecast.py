55d2e2173d239fbf8b3870ab986612b6ab5179d1
dangiiwa <belloahmad@ymail.com>
2022-06-09 15:26:23 +0100

    Update favarx_forecast.py

--- favarx_forecast.py
+++ favarx_forecast.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.decomposition import PCA
 from statsmodels.tsa.api import VAR
 
-
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 
@@ -20,58 +19,47 @@ def favarx_forecast():
     "1","2","3","4"
     )
 
-    #
-    with st.sidebar.form("my_forecast"):
-        st.write("Select Exogenous Values")
+    st.sidebar.write("Select Exogenous Values")
 
-        # quarter = st.selectbox("Quarter", quarters)
-        quarter = st.number_input("Number of Quarters", 1, 4, 4)
-        COP = st.slider("Crude Oil Price", 0, 120, 50)
-        GXP = st.slider("GXP", 1000000, 99999999, 4024610)
-        MPMIS = st.slider("MPMIS", 0, 100, 51)
+    quarter = st.sidebar.number_input("Number of Quarters", 1, 4, 4)
+    COP = st.sidebar.slider("Crude Oil Price", 0, 120, 50)
+    GXP = st.sidebar.slider("GXP", 1000000, 99999999, 4024610)
+    MPMIS = st.sidebar.slider("MPMIS", 0, 100, 51)
 
-        # checkbox_val = st.checkbox("Form checkbox")
+    exoginputdata = {'COP':[COP, COP, COP, COP],
+    'GXP': [GXP, 4281500, 4367130, 4452760],
+    'MPMIS': [MPMIS, 52, 52, 53]}
 
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Forecast")
-        if submitted:
-            exoginputdata = {'COP':[COP, COP, COP, COP],
-            'GXP': [GXP, 4281500, 4367130, 4452760],
-            'MPMIS': [MPMIS, 52, 52, 53]}
+    exog = dflog[['COP', 'MPMIS', 'GXP']]
+    exog.drop(exog.tail(4).index,inplace=True) # drop last n rows
+    # print(exog)
 
-            exog = dflog[['COP', 'MPMIS', 'GXP']]
-            exog.drop(exog.tail(4).index,inplace=True) # drop last n rows
-            # print(exog)
+    input_exog = pd.DataFrame(exoginputdata, index=df[-4:].index)
+    input_exog = np.log(input_exog)
+    # print(input_exog)
 
-            input_exog = pd.DataFrame(exoginputdata, index=df[-4:].index)
-            input_exog = np.log(input_exog)
-            # print(input_exog)
+    frames = [exog, input_exog]
+    exogOI = pd.concat(frames)
+    # print(exogOI)
 
-            frames = [exog, input_exog]
-            exogOI = pd.concat(frames)
-            # print(exogOI)
+    shiftofgxp = exogOI['GXP'] - exogOI['GXP'].shift(4)
+    #print(shiftofgxp)
 
-            shiftofgxp = exogOI['GXP'] - exogOI['GXP'].shift(4)
-            # print(shiftofgxp)
+    exogOI = exogOI[['COP', 'MPMIS']]
+    exogOI = pd.merge(exogOI, shiftofgxp, on=['date'])
+    # print(exogOI)
 
-            exogOI = exogOI[['COP', 'MPMIS']]
-            exogOI = pd.merge(exogOI, shiftofgxp, on=['date'])
-            # print(exogOI)
+    exogModel = exogOI.loc['2017-03-31':'2021-12-31', ['COP', 'GXP', 'MPMIS']]
+    # print(exogModel)
 
-            exogModel = exogOI.loc['2017-03-31':'2021-12-31', ['COP', 'GXP', 'MPMIS']]
-            # print(exogModel)
+    exogForecast = exogOI.loc['2022-03-31':'2022-12-31', ['COP', 'GXP', 'MPMIS']]
+    # print(exogForecast)
 
-            exogForecast = exogOI.loc['2022-03-31':'2022-12-31', ['COP', 'GXP', 'MPMIS']]
-            # print(exogForecast)
+    var = VAR(endog, exogModel)
+    results = var.fit(1)
 
-            var = VAR(endog, exogModel)
-            results = var.fit(1)
-
-            forecast = results.forecast(endog.values[-1:], steps=4, exog_future=exogForecast)
-            forecastdf = pd.DataFrame(data = forecast, columns = ['pc1', 'pc2', 'pc3', 'pc4', 'pc5', 'dlRY'], index=df[-4:].index)
-
-
-    # st.write("Outside the form")
+    forecast = results.forecast(endog.values[-1:], steps=4, exog_future=exogForecast)
+    forecastdf = pd.DataFrame(data = forecast, columns = ['pc1', 'pc2', 'pc3', 'pc4', 'pc5', 'dlRY'], index=df[-4:].index)
 
     col1, col2 = st.columns([3, 2])
     # col1.subheader("Line chart of forecast values")
